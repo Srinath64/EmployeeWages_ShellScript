f8b05c59d3e946b8b0b9d8a9d19ab98b424167bc
Srinath S <connect2srinath06@gmail.com>
2023-02-04 08:49:15 +0530

    Employee Daily Wage

--- Employee.sh
+++ Employee.sh
@@ -5,7 +5,10 @@ randomCheck=$((RANDOM%2));
 
 if [ $isPresent -eq $randomCheck ]
 then
-	echo "Employee is Present";
+	empRatePerHr=20;
+	empHrs=8;
+	salary=$(($empHrs*$empRatePerHr));
+	echo $salary;
 else
-	echo "Employee is Absent";
+	echo $salary=0;
 fi
